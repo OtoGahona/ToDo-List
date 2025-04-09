51056a51db6d37f167239ed6cb7bdd287e35e173
OtoGahona <gahonaotoniel50@gmail.com>
2025-04-08 22:12:17 -0500

    Add files via upload

---
+++ ToDo-List/src/main/java/com/work_list/ToDo_List/ToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.work_list.ToDo_List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListApplication.class, args);
+	}
+
+}
---
+++ ToDo-List/src/test/java/com/work_list/ToDo_List/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work_list.ToDo_List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
